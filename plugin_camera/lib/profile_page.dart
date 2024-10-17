e7738281602a95e51a67e9fad427c53867d26edd
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-10-18 01:35:37 +0700

    edit profile page

--- plugin_camera/lib/profile_page.dart
+++ plugin_camera/lib/profile_page.dart
@@ -7,42 +7,45 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('Profile'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://example.com/profile.jpg'), // Ganti dengan URL foto profil
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Nama Pengguna',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'email@contoh.com',
-              style: TextStyle(fontSize: 16, color: Colors.grey),
-            ),
-            SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {
-                // Tindakan untuk mengedit profil
-              },
-              child: Text('Edit Profil'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Tindakan untuk logout
-              },
-              child: Text('Logout'),
-            ),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/contoh_profile.jpg'), // Ganti dengan URL foto profil
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Nama Pengguna',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'email@contoh.com',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: () {
+                  // Tindakan untuk mengedit profil
+                },
+                child: const Text('Edit Profil'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Tindakan untuk logout
+                },
+                child: const Text('Logout'),
+              ),
+            ],
+          ),
         ),
       ),
     );
