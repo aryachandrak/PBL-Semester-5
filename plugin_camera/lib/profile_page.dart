b24725a0c31247718e5cda6e357484618acb114a
Putri A. Aliciawati <aliciaputri706@gmail.com>
2024-10-18 02:07:16 +0700

    edit layout profile page

--- plugin_camera/lib/profile_page.dart
+++ plugin_camera/lib/profile_page.dart
@@ -6,47 +6,102 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/contoh_profile.jpg'), // Ganti dengan URL foto profil
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'Nama Pengguna',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'email@contoh.com',
-                style: TextStyle(fontSize: 16, color: Colors.grey),
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: () {
-                  // Tindakan untuk mengedit profil
-                },
-                child: const Text('Edit Profil'),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Tindakan untuk logout
-                },
-                child: const Text('Logout'),
-              ),
-            ],
-          ),
+        title: const Text('Profile', style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            // Tindakan kembali
+          },
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.black),
+            onPressed: () {
+              // Tindakan log out
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings, color: Colors.black),
+            onPressed: () {
+              // Tindakan pengaturan
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 16),
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/contoh_profile.jpg'), // Ganti dengan jalur gambar avatar
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'Nama Pengguna',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'email@contoh.com',
+            style: TextStyle(fontSize: 14, color: Colors.grey),
+          ),
+          const SizedBox(height: 24),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text('Account'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // Tindakan untuk Account
+                  },
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.notifications),
+                  title: const Text('Notifications'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // Tindakan untuk Notifications
+                  },
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.history),
+                  title: const Text('Scan History'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // Tindakan untuk Devices
+                  },
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.lock),
+                  title: const Text('Passwords'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // Tindakan untuk Passwords
+                  },
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.language),
+                  title: const Text('Language'),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  onTap: () {
+                    // Tindakan untuk Language
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
