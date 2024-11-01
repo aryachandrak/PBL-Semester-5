04184b6c9be67b803d8158f8f5271c7436eeeaa8
Arya Chandra <aryachandrak14@gmail.com>
2024-11-01 08:30:33 +0700

    State management history page done

--- plugin_camera/lib/main.dart
+++ plugin_camera/lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:plugin_camera/provider/history_provider.dart';
 import 'package:plugin_camera/views/main_page.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final cameras = await availableCameras();
-  runApp(MainApp(
-    cameras: cameras,
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (_) => HistoryProvider()),
+    ],
+    child: MainApp(cameras: cameras),
   ));
 }
 
