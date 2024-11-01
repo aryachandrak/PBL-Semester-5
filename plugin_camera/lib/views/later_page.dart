6f3808ab76532eeb1dcfa9031aac57cf82d081b0
Jantra Buana <jantralanglangbuana50@gmail.com>
2024-11-01 10:18:37 +0700

    penambahan komunitas

--- plugin_camera/lib/views/later_page.dart
+++ plugin_camera/lib/views/later_page.dart
@@ -1,29 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
-class LaterPage extends StatefulWidget {
-  const LaterPage({super.key});
 
-  @override
-  State<LaterPage> createState() => _LaterPageState();
-}
+class LaterPage extends StatelessWidget {
+  final List<Map<String, String>> images = [
+    {'path': 'assets/scan1.jpg', 'description': 'Deskripsi gambar 1'},
+    {'path': 'assets/scan2.jpeg', 'description': 'Deskripsi gambar 2'},
+    // Tambahkan lebih banyak gambar dan deskripsi sesuai kebutuhan
+  ];
 
-class _LaterPageState extends State<LaterPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Scaffold(
         appBar: AppBar(
           title: Text(
-            "Later Fitur",
+            "Fitur Proses",
             style: GoogleFonts.montserrat(fontSize: 20),
           ),
           backgroundColor: Color.fromARGB(255, 0, 255, 238),
         ),
-        body: Center(
-          child: Text("Untuk fitur selanjutnya"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 0.7,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+          ),
+          itemCount: images.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                _showImageDescription(context, images[index]['description']!);
+              },
+              child: CachedNetworkImage(
+                imageUrl: images[index]['path']!,
+                placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+                fit: BoxFit.cover,
+              ),
+            );
+          },
         ),
       ),
+    ),
+  );
+}
+
+  void _showImageDescription(BuildContext context, String description) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Deskripsi Gambar'),
+          content: Text(description),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Tutup'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
