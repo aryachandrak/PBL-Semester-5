b61fbc98a133464772bb6f409519b8996fac5997
Elis Nurhidayati <elisnurhidayati777@gmail.com>
2024-12-17 16:07:40 +0700

    responsif login

--- app_flutter/lib/views/auth/login_page.dart
+++ app_flutter/lib/views/auth/login_page.dart
@@ -98,247 +98,257 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Logo
-            Center(
-              child: Column(
-                children: [
-                  Container(
-                    height: 120,
-                    width: 120,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape
-                          .circle, // Opsional: Membuat gambar berbentuk lingkaran
-                    ),
-                    child: Image.asset(
-                      'assets/logoCoba.png', // Path gambar lokal
-                      fit: BoxFit.cover, // Atur sesuai kebutuhan
-                      errorBuilder: (BuildContext context, Object error,
-                          StackTrace? stackTrace) {
-                        return const Icon(
-                          Icons.error, // Ikon fallback jika gambar gagal dimuat
-                          size: 40,
-                          color: Colors.red,
-                        );
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Text(
-                    'Hello There!',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "Let's glow together with us",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 32),
-            // Nama Field
-// Email Field
-            TextField(
-              controller: email,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.email_outlined),
-                labelText: 'Email',
-                floatingLabelStyle: const TextStyle(color: Color(0xFF6DA06F)),
-                labelStyle: const TextStyle(color: Colors.grey),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.grey, // Warna border abu-abu saat tidak fokus
-                    width: 2.0, // Ketebalan border
-                  ),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.grey, // Warna border abu-abu saat fokus
-                    width: 3.0, // Ketebalan border lebih tebal saat fokus
-                  ),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 16),
-// Password Field
-            TextField(
-              controller: password,
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.lock_outline),
-                labelText: 'Password',
-                floatingLabelStyle: const TextStyle(color: Color(0xFF6DA06F)),
-                labelStyle: const TextStyle(color: Colors.grey),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.grey, // Warna border abu-abu saat tidak fokus
-                    width: 2.0, // Ketebalan border
-                  ),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(
-                    color: Colors.grey, // Warna border abu-abu saat fokus
-                    width: 3.0, // Ketebalan border lebih tebal saat fokus
-                  ),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
-            ),
-            //Lupa password
-            Align(
-              alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ForgotPassword()));
-                  },
-                  style: ElevatedButton.styleFrom(
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent),
-                  child: const Text(
-                    'Lupa Password?',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Color.fromARGB(255, 46, 93, 64),
-                    ),
-                  )),
-            ),
-            const SizedBox(height: 18),
-            // Tombol Lanjutkan
-            GestureDetector(
-              onTap: () async {
-                setState(() {
-                  isLoading = true;
-                });
-                await signIn();
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFF6DA06F), // Warna hijau pada tombol
-                  borderRadius: BorderRadius.circular(16), // Sudut membulat
-                ),
-                padding: const EdgeInsets.symmetric(
-                    vertical: 14), // Padding dalam tombol
-                child: const Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white, // Warna teks putih
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 24),
-            // Divider atau pemisah
-            Row(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey[400],
-                    thickness: 1,
+                // Logo
+                const SizedBox(height: 80),
+                Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 120,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape
+                              .circle, // Opsional: Membuat gambar berbentuk lingkaran
+                        ),
+                        child: Image.asset(
+                          'assets/logoCoba.png', // Path gambar lokal
+                          fit: BoxFit.cover, // Atur sesuai kebutuhan
+                          errorBuilder: (BuildContext context, Object error,
+                              StackTrace? stackTrace) {
+                            return const Icon(
+                              Icons
+                                  .error, // Ikon fallback jika gambar gagal dimuat
+                              size: 40,
+                              color: Colors.red,
+                            );
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        'Hello There!',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        "Let's glow together with us",
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Text(
-                    'Atau',
-                    style: TextStyle(color: Colors.grey),
+                const SizedBox(height: 32),
+                // Nama Field
+// Email Field
+                TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.email_outlined),
+                    labelText: 'Email',
+                    floatingLabelStyle:
+                        const TextStyle(color: Color(0xFF6DA06F)),
+                    labelStyle: const TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors
+                            .grey, // Warna border abu-abu saat tidak fokus
+                        width: 2.0, // Ketebalan border
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.grey, // Warna border abu-abu saat fokus
+                        width: 3.0, // Ketebalan border lebih tebal saat fokus
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
                   ),
                 ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey[400],
-                    thickness: 1,
+
+                const SizedBox(height: 16),
+// Password Field
+                TextField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.lock_outline),
+                    labelText: 'Password',
+                    floatingLabelStyle:
+                        const TextStyle(color: Color(0xFF6DA06F)),
+                    labelStyle: const TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors
+                            .grey, // Warna border abu-abu saat tidak fokus
+                        width: 2.0, // Ketebalan border
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.grey, // Warna border abu-abu saat fokus
+                        width: 3.0, // Ketebalan border lebih tebal saat fokus
+                      ),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                ),
+                //Lupa password
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ForgotPassword()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent),
+                      child: const Text(
+                        'Lupa Password?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Color.fromARGB(255, 46, 93, 64),
+                        ),
+                      )),
+                ),
+                const SizedBox(height: 18),
+                // Tombol Lanjutkan
+                GestureDetector(
+                  onTap: () async {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    await signIn();
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF6DA06F), // Warna hijau pada tombol
+                      borderRadius: BorderRadius.circular(16), // Sudut membulat
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 14), // Padding dalam tombol
+                    child: const Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white, // Warna teks putih
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                // Divider atau pemisah
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey[400],
+                        thickness: 1,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        'Atau',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey[400],
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                // Tombol Google Login
+                GestureDetector(
+                  onTap: () {
+                    googleLogin();
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network(
+                        'https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA', // Path logo Google
+                        width: 24,
+                        height: 24,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        'Login dengan Google',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 32),
+                // Footer
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Belum punya akun? ',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignUpPage()),
+                          );
+                        },
+                        child: const Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF6DA06F),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 24),
-            // Tombol Google Login
-            GestureDetector(
-              onTap: () {
-                googleLogin();
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.network(
-                    'https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA', // Path logo Google
-                    width: 24,
-                    height: 24,
-                  ),
-                  const SizedBox(width: 8),
-                  Text(
-                    'Login dengan Google',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 32),
-            // Footer
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Belum punya akun? ',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUpPage()),
-                      );
-                    },
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF6DA06F),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
